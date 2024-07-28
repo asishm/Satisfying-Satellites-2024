14b0bcbdf696d65b004d10f6388872dd18db1875
AdithyaKrishna <adithyakrishnanszz@gmail.com>
2024-07-28 15:14:48 +0530

    made files for questions

--- satisfying_satellites/trivia/cats.py
+++ satisfying_satellites/trivia/cats.py
@@ -16,34 +16,4 @@ questions = [
         question="Who was the most favorite mayor of the small Alaskan town Talkeetna?",
         answers=["Stubbs the Cat", "Mayor Stubbs, the cat", "a cat", "a good kitty"],
     ),
-    Question(
-        category="dogs",
-        question="Why do dogs dig holes?",
-        answers=["Swimming pond", "Treasure Hunting", "Bury Bones", "Kill Ants"],
-    ),
-    Question(
-        category="dogs",
-        question="Which song genre do dogs like?",
-        answers=["Ruff song", "Bark song", "Roof song", "Pup song"],
-    ),
-    Question(
-        category="dogs",
-        question="Do dogs like chasing tails?",
-        answers=["Yes, they do", "Tail chase", "Tale cheese", "No, they don't"],
-    ),
-    Question(
-        category="babies",
-        question="Why don't babies keep secrets?",
-        answers=["Because they might leak!", "They can't talk", "They don't trust anyone", "They forget"],
-    ),
-    Question(
-        category="babies",
-        question="What is the most common first word spoken by babies?",
-        answers=["Mama", "Dada", "Ball", "Food"],
-    ),
-    Question(
-        category="babies",
-        question="What do babies do with thier toes?",
-        answers=["Try to eat them", "Nothing", "Paint them", "Kick You"],
-    ),
 ]
